b35c8e1c8e1ac2ae088c1da1d948b72b701d705f
Kristen Kirchgesler <kristen.arbutiski@gmail.com>
2018-06-23 19:11:57 -0400

    initial upload

---
+++ .neutrinorc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  use: [
+    '@neutrinojs/standardjs',
+    [
+      '@neutrinojs/react',
+      {
+        html: {
+          title: 'elite-sheet'
+        }
+      }
+    ],
+    '@neutrinojs/jest'
+  ]
+};
